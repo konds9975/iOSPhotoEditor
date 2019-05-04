7d213ff42d4fba80014434f12584d48528d76380
kondya <konds9975@gmail.com>
2019-05-04 13:36:44 +0530

    Initial Commit

---
+++ iOSPhotoEditor/iOSPhotoEditor.h
@@ -0,0 +1,19 @@
+//
+//  iOSPhotoEditor.h
+//  iOSPhotoEditor
+//
+//  Created by Kondya on 04/05/19.
+//  Copyright © 2019 Kondya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSPhotoEditor.
+FOUNDATION_EXPORT double iOSPhotoEditorVersionNumber;
+
+//! Project version string for iOSPhotoEditor.
+FOUNDATION_EXPORT const unsigned char iOSPhotoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPhotoEditor/PublicHeader.h>
+
+
